e3c97acfd4c31b8e2df951ea349e855b8972b36b
Rifat <rassulow2012@yandex.ru>
2022-12-01 00:35:07 +0400

    fix errors

--- src/main/java/com/homework/course_work/repo/LoanOfBooksRepository.java
+++ src/main/java/com/homework/course_work/repo/LoanOfBooksRepository.java
@@ -1,9 +1,10 @@
 package com.homework.course_work.repo;
 
+import com.homework.course_work.entities.LoanOfBooks;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoanOfBooks extends JpaRepository<LoanOfBooks, Integer> {
+public interface LoanOfBooksRepository extends JpaRepository<LoanOfBooks, Integer> {
 
 }
