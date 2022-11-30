17e3dfcb15aab2958e855a5d7f7cac9b9dae40e5
Rifat <rassulow2012@yandex.ru>
2022-12-01 02:50:05 +0400

    add work with files

--- src/main/java/com/homework/course_work/controllers/MainController.java
+++ src/main/java/com/homework/course_work/controllers/MainController.java
@@ -1,13 +1,52 @@
 package com.homework.course_work.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class MainController {
 
+    @Value("${upload.path}")
+    private String pathToFile;
+
     @GetMapping("/all-books")
-    public String hello() {
+    public String books(Model model) {
+        File[] files = new File(pathToFile).listFiles();
+
+        model.addAttribute("files", files);
+
+        return "index";
+    }
+
+    @PostMapping("/all-books")
+    public String addImg(
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
+
+        if (file != null) {
+            File uploadFile = new File(pathToFile);
+
+            if (!uploadFile.exists()) {
+                uploadFile.mkdir();
+            }
+
+//            String uuidFile = UUID.randomUUID().toString();
+//            String resultFilename = uuidFile + "." + ;
+
+//            file.transferTo(new File(pathToFile + file.getOriginalFilename()));
+//            File.createTempFile(file.getpathToFile)
+//            File file1 = new File(pathToFile + file.getOriginalFilename());
+        }
+
         return "index";
     }
 }
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -6,6 +6,30 @@
 <body>
 <div>
     <h4>Пока</h4>
+
+    <#if files?has_content>
+        <#list files as file>
+            <a href="index.ftl">
+                <img src="/uploads/${file.name}" width="300" height="300" alt="Изображение не отображается">
+            </a>
+        </#list>
+    <#else>
+        <span>В библиотеке отсутствуют изображения</span>
+    </#if>
+    <div>
+        <form enctype="multipart/form-data" method="post">
+            <p><input type="file" name="file">
+                <input type="submit" value="Отправить"></p>
+        </form>
+    </div>
+    <div>
+        <div>
+            <a href="/">Добавить читателя</a>
+        </div>
+        <div>
+            <a href="/">Добавить книгу</a>
+        </div>
+    </div>
 </div>
 </body>
 </head>
